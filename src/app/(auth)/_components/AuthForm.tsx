e930975668b3dd41030143d9123f1dd301548f89
JwanCortez93 <jwancortez@gmail.com>
2024-06-19 01:39:07 -0300

    appwrite integration

--- src/app/(auth)/_components/AuthForm.tsx
+++ src/app/(auth)/_components/AuthForm.tsx
@@ -14,6 +14,7 @@ import CustomFormField from "./CustomFormField";
 import { authFormSchemaSignIn, authFormSchemaSignUp } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { signIn, signUp } from "../actions/users";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -44,16 +45,16 @@ const AuthForm = ({ type }: { type: string }) => {
 
     try {
       if (type === "sign-up") {
-        // const newUser = await signUp(data);
-        // setUser(newUser);
+        const newUser = await signUp(data);
+        setUser(newUser);
       } else {
-        // const response = await signIn({
-        //   email: data.email,
-        //   password: data.password,
-        // });
-        // if (response) {
-        //   router.push("/");
-        // }
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        if (response) {
+          router.push("/");
+        }
       }
     } catch (error) {
       console.log(error);
@@ -193,7 +194,7 @@ const AuthForm = ({ type }: { type: string }) => {
                 ) : type === "sign-in" ? (
                   "Sign In"
                 ) : (
-                  "Sign Un"
+                  "Sign Up"
                 )}
               </Button>
             </form>
